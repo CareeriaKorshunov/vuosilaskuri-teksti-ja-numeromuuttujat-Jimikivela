324a4d644c02f95eba18dccf67f7002d2114145e
Jimi Kivelä <jimi.kivela@student.careeria.fi>
2024-09-25 10:45:03 +0300

    tehtävä

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,8 +4,23 @@
     {
         public static void Main(string[] args)
         {
-            //Koodi tähän
-           
+            //datetime
+            DateTime d = DateTime.Now;
+            //muuttujat
+            int ika;
+            int vuosi = d.Year;
+            int syntymavuosi = 2007;
+            int tavoiteaika = 25;
+            int tavoitevuosi;
+
+            string nimi = "Jimi";
+
+            //lasketaan ikä ja tavoite vuosi;
+            ika = vuosi - syntymavuosi;
+            tavoitevuosi = syntymavuosi + tavoiteaika;
+
+            //tulostetaan konsoliin
+            Console.WriteLine($"Nimeni on {nimi}. Ikäni on {ika} ja syntymävuoteni on {syntymavuosi} ja vuonna {tavoitevuosi} täytän {tavoiteaika} vuotta!");
 
         }
     }
